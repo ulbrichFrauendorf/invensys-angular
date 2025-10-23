ccdd116e674572a3f22f00ba1fdb866919bb19b4
Ulbrich Frauendorf <ulbrich@invensys.web.za>
2025-10-24 00:17:51 +0200

    Correct exports

--- projects/invensys-angular-shared/src/public-api.ts
+++ projects/invensys-angular-shared/src/public-api.ts
@@ -2,20 +2,24 @@
  * Public API Surface of invensys-angular-shared
  */
 
-/* Components */
+/* Components - organized by folder structure */
 export * from './lib/components/button/button.component';
-export * from './lib/components/input-text/input-text.component';
-export * from './lib/components/select/select.component';
-export * from './lib/components/multi-select/multi-select.component';
+export * from './lib/components/card/card.component';
+export * from './lib/components/checkbox/checkbox.component';
 export * from './lib/components/chip/chip.component';
 export * from './lib/components/chips/chips.component';
-export * from './lib/components/card/card.component';
-export * from './lib/components/dialog/dialog.component';
 export * from './lib/components/confirmation-dialog/confirmation-dialog.component';
-export * from './lib/components/checkbox/checkbox.component';
-
+export * from './lib/components/confirmation-dialog/services/confirmation-dialog.service';
+export * from './lib/components/dialog/dialog.component';
+export * from './lib/components/dialog/dialog-base';
+export * from './lib/components/dialog/inner/dialog-actions/dialog-actions.component';
+export * from './lib/components/dialog/services/dialog.service';
+export * from './lib/components/dialog/services/dialog.interfaces';
 export * from './lib/components/empty-state/empty-state.component';
 export * from './lib/components/empty-state-table/empty-state-table.component';
+export * from './lib/components/input-text/input-text.component';
+export * from './lib/components/multi-select/multi-select.component';
+export * from './lib/components/select/select.component';
 
 /* Directives */
 export * from './lib/directives/tooltip/tooltip.component';
@@ -23,9 +27,7 @@ export * from './lib/directives/tooltip/tooltip.directive';
 
 /* Services */
 export * from './lib/services/data-update-event/data-update-event.service';
-export * from './lib/components/confirmation-dialog/services/confirmation-dialog.service';
-export * from './lib/components/dialog/services/dialog.service';
-export * from './lib/components/dialog/services/dialog.interfaces';
 
 /* Utils */
 export * from './lib/utils/uniquecomponentid';
+export * from './lib/utils/zindexutils';
